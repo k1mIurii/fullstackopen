e208ef08f2f4180c5193cd26be8669a0bba6ed23
yura <kyy101000@gmail.com>
2025-05-25 12:24:33 -0500

    4.12

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -13,11 +13,15 @@ blogRouter.get('/', async (request, response) => {
 blogRouter.post('/', async (request, response) => {
   const body = request.body
 
+  if (!body.title || !body.url) {
+    return response.status(400).end()
+  }
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes || 0
   })
 
   try {
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -25,6 +25,61 @@ test('Correct amount of blogs', async () => {
   assert.strictEqual(response.body.length, helper.blogs.length)
 })
 
+test('Unique identifier', async () => {
+  assert(helper.blogs[0].hasOwnProperty('id'))
+})
+
+test('Successfull creation of new blog', async () => {
+  const newBlog = {
+    title: "Who killed Mark?",
+    author: "Oxxxymiron",
+    url: "vk.com",
+    likes: 12
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogs = await Blog.find({})
+  assert.strictEqual(blogs.length, helper.blogs.length + 1)
+})
+
+test('Like property is missing, default was used', async () => {
+  const newBlog = {
+    title: "Who killed Mark?",
+    author: "Oxxymiron",
+    url: "vk.com"
+  }
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(response.body.likes, 0)
+})
+
+test('Missing title or url produces 400', async () => {
+  const newBlog = {
+    title: "Who killed Mark?",
+    author: "Oxxxymiron",
+    likes: 5
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsFromDB = await Blog.find({})
+  console.log(blogsFromDB.length)
+  assert.strictEqual(blogsFromDB.length, helper.blogs.length)
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
